5caf5b47089547a81b670fca90f2e41f306a91e0
Rahul Niraula <rahul.niraula7@live.com>
2020-05-03 17:59:42 +0545

    removed unnecessay config

--- src/Config/admin.php
+++ src/Config/admin.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Models\Contact\Contact;
-use App\Models\Contact\ContactType;
-use Illuminate\Database\Schema\Blueprint;
-
 return [
     'prefix' => 'admin',
-
-    'crud_classes' => [
-        ContactType::class,
-        Contact::class
-    ],
+    'crud_classes' => [],
     'base_admin_layout' => 'admin::default_layout'
 ];
