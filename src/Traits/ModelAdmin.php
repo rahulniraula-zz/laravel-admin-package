1223324deacec88c53658bf7daa7ce7fb36f570d
Rahul Niraula <rahul.niraula7@live.com>
2020-05-03 18:38:44 +0545

    moved common utils to trait

---
+++ src/Traits/ModelAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Geeklearners\Traits;
+
+trait ModelAdmin
+{
+    public static $form_fields = [];
+    public static $form_submit_button = ['type' => 'button', 'value' => "Submit", 'class' => 'btn btn-primary'];
+    public static $form_update_button = ['type' => 'button', 'value' => "Update", 'class' => 'btn btn-primary'];
+
+
+    public function getValue($col, $method)
+    {
+        if (method_exists($this, $method)) {
+            $d = $this->$method();
+            // dd($d);
+            if (isset($d[$col]) && is_callable($d[$col])) {
+                return ($d[$col])($this);
+            }
+        }
+        return $this->$col ?? '';
+    }
+}
